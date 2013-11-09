1a276a98068632182781c434c93978ffdcf74c1a
Peder Jakobsen <pjakobsen@gmail.com>
2013-11-08 19:08:47 -0500

    Moved iati download method to iati.py

--- etl/iati.py
+++ etl/iati.py
@@ -8,6 +8,44 @@ from lxml import objectify
 from data_sources import datadir
 from petl import fromcsv, look, head
 
+def download_iati_metadata():
+    sample_sets="http://www.iatiregistry.org/api/search/dataset?filetype=activity&offset=1000&limit=10"
+    iati_files=[]
+    if sample_sets:
+        req = urllib2.Request(sample_sets)
+    else:
+        req = urllib2.Request(iati_registry['datasets'])
+    opener = urllib2.build_opener()
+    f = opener.open(req)
+    ds = json.load(f)
+    print ds
+    for d in ds['results']:
+        print d
+        dataset_url = "{}{}".format("http://www.iatiregistry.org/api/rest/dataset/",d)
+        print dataset_url
+        
+        req = urllib2.Request(dataset_url)
+        f = opener.open(req)
+        package = json.load(f)
+        print package
+        for i,p in enumerate(package['resources']):
+            #print i+1,p['url']
+            iati_files.append((i,p['url']))
+    
+    print iati_files
+    fp = open(datadir+'iati.csv','w')
+    fp.write('\n'.join('%s %s' % i for i in iati_files))   
+    fp.close() # you can omit in most cases as the destructor will call if
+    
+    
+def iati():
+
+    req = urllib2.Request(iati_registry['datasets'])
+    opener = urllib2.build_opener()
+    f = opener.open(req)
+    ds = json.load(f)
+    tbl = etl(zip(ds)).pushheader(['dataset'])
+    print look(tbl)
         
 def xmlfiles():
     ''' Traverse through every XML document and put 
