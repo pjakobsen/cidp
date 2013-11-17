f169734e0188edaf0eddb21567cd4752dcd1b54e
Peder Jakobsen <pjakobsen@gmail.com>
2013-11-17 12:01:35 -0500

    Downloading all CIDA project files using urllib.urlretrieve

--- etl/cida/download_cida.py
+++ etl/cida/download_cida.py
@@ -15,7 +15,21 @@ This visualization is based on two main data sources. The CIDA project browser (
 '''
 
 import os
+import urllib
 
-dowload_dir = os.normapth("~/temp/cida")
+urllib.urlretrieve("http://www.acdi-cida.gc.ca/cidaweb/cpo.nsf/vLUOpenDataFile/PBOpenData/\$file/Project%20Browser%20English.csv","browser.csv")
+print "OK Browser file"
+urllib.urlretrieve("http://www.acdi-cida.gc.ca/INET/IMAGES.NSF/vLUImages/Open%20Data/\$file/HPDS-2011-2012-eng.csv","hpds-2012.csv")
+print "OK 2012"
+urllib.urlretrieve("http://www.acdi-cida.gc.ca/INET/IMAGES.NSF/vLUImages/Open%20Data/\$file/HPDS-2010-2011-eng.csv","hpds-2011.csv")
+print "OK 2011"
+urllib.urlretrieve("http://www.acdi-cida.gc.ca/INET/IMAGES.NSF/vLUImages/Open%20Data/\$file/HPDS-2009-2010-eng.csv","hpds-2010.csv")
+print "OK 2010"
+urllib.urlretrieve("http://www.acdi-cida.gc.ca/INET/IMAGES.NSF/vLUImages/Open%20Data/\$file/HPDS-2008-2009-eng.csv","hpds-2009.csv")
+print "OK 2009"
+urllib.urlretrieve("http://www.acdi-cida.gc.ca/INET/IMAGES.NSF/vLUImages/Open%20Data/\$file/HPDS-2007-2008-eng.csv","hpds-2008.csv")
+print "OK 2008"
+urllib.urlretrieve("http://www.acdi-cida.gc.ca/INET/IMAGES.NSF/vLUImages/Open%20Data/\$file/HPDS-2006-2007-eng.csv","hpds-2007.csv")
+print "ALL OK"
 
 
