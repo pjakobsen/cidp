95cf0fa3e4b853502776f474430f58ceafa56d81
Peder Jakobsen <pjakobsen@gmail.com>
2013-12-10 11:46:30 -0500

    Mashing two api calls

--- dashboard/scripts/d3data.py
+++ dashboard/scripts/d3data.py
@@ -23,13 +23,22 @@ def main():
     mort = cut(mort,"Country Name","1973","2006","2007","2008","2009","2010","2011","2012")
     mort = petl.rename(mort, 'Country Name', 'country')
     mort_rates = facetcolumns(mort, 'country')
-    pprint(mort_rates['Malaysia'])
+    def getmort(c,y):
+        try:
+          return mort_rates[c][y]  
+        except:
+          return ''
+        
+    ''' faceting allows us to do for example:
+    pprint(mort_rates['Malaysia']['2009'])
     pprint(mort_rates['Denmark'])
-    pprint(mort_rates['Japan'])
-    
-    sys.exit()
+   
+    '''
+
     f = open('mnhc.ids')
-    base_url = 'http://cidp.herokuapp.com/cube/cida/facts?cut=project_number:'#A033033007
+    base_url = 'http://localhost:7000/cube/cida/'
+    fact_url = base_url+'facts?cut=project_number:'#A033033007
+    aggregate_url = base_url+ 'aggregate?cut=project_number:'#A033033007
     bads = ['Africa MC','Americas MC','Asia MC','Europe MC']
     renamethis={'Afghanistan TIS':'Afghanistan',
                 'Tanzania,Un Rep':'Tanzania',
@@ -41,47 +50,34 @@ def main():
     for example there are 6 entries for #A033033007
     '''
     dat=[]
-    for id in list(f)[0:1]:
-        print base_url + id
-        project_data = json.load(urllib2.urlopen(base_url + id))
-        print renamethis.keys()
-        for p in project_data:
-            c= p['country_region_name']
-            country =  renamethis[c] if  c in renamethis.keys() else c
-            #mortality_rates = 
-            if country not in bads:
-                dat.append( {
-                      'id':p['id'],
-                      'project':p['project_number'],
-                      'year':p['fiscal_year'],
-                      'continent':p['continent_name'],
-                      'country':country,
-                      'max':int(round(p['maximum_cida_contribution'])),
-                      'spent':int(round(p['amount_spent']))}
-                      
-                )
-                
-    c = petl.fromdicts(dat) 
+    for id in list(f)[0:-1]:
 
-    
-    '''
-    browser =  fromcsv("../../etl/cida/data/Project Browser English.csv")
+        project_data = json.load(urllib2.urlopen(fact_url + id))
+         # now we can append aggregates
+        aggr = json.load(urllib2.urlopen(aggregate_url + id))['summary']
+        # just grab the first record
+        #for p in project_data:
+        p=project_data[0]
+        c= p['country_region_name']
+        country =  renamethis[c] if  c in renamethis.keys() else c
 
-    browser = skip(browser, 1)
-    pprint(header(browser))
-    ex()
-    browser = rename(browser, "Country","country")
-    browser = cut(browser, "country","Maximum CIDA Contribution")
-    print header(browser)
-    print look(browser)
-    new = petl.lookupjoin(c,mort,"country")
-    '''
-    
-    
-    
-    print look(new)
-    sys.exit()
-    petl.tojson(new, 'mashup.json')
+        if country not in bads:
+            dat.append( {
+                  'id':p['id'],
+                  'project':p['project_number'],
+                  'year':p['fiscal_year'],
+                  'continent':p['continent_name'],
+                  'country':country,
+                  'max':int(round(p['maximum_cida_contribution'])),
+                  'spent':int(round(aggr['amount_spent_sum'])),
+                  'mortality_rate':getmort(c,p['fiscal_year'])}
+                  
+            )
+       
+    comb = petl.fromdicts(dat) 
+
+    print look(comb)
+    petl.tojson(comb, 'mashup.json')
 
 if __name__ == '__main__':
 	main()
