ebaea461725173ed5a42cefad71a497813b21c7a
Peder Jakobsen <pjakobsen@gmail.com>
2013-12-30 16:15:43 -0500

    More dashboard code in Flask and Javascript to get Recline.js going

--- dashboard/sandbox/app/views.py
+++ dashboard/sandbox/app/views.py
@@ -1,9 +1,13 @@
-from flask import render_template, flash, redirect
+# encoding: utf-8
+from flask import render_template, flash, redirect,jsonify, Response
 from app import app,db, models
 from app import conn
 from forms import LoginForm
 from models import Webuser, ROLE_USER, ROLE_ADMIN
 import cubes
+from petl import *
+from pprint import pprint
+
 
 MODEL_PATH = "model.json"
 DB_URL = "sqlite:///data.sqlite"
@@ -35,11 +39,47 @@ def index():
 def report(dim_name=None):
     return "Hello " + str(dim_name)
     
+@app.route('/mnch.csv')
+def generate_full_csv():
+    sql = "select full_project_number from project where id in (select project_id from initiative_project where initiative_id=1)"
+    table = fromdb(conn, sql)
+    pn = itervalues(table, 'full_project_number')
+    fields="fiscal_year, project_number, title, country, maximum_cida_contribution, amount_spent"
+    ids = [p for p in pn]
+    id_strings = "'"+"','".join(ids)+"'"
+    sql = "select {} from cida where project_number in ({})".format(fields,id_strings)
+    
+    table = fromdb(conn, sql)
+
+    def generate():
+        yield  fields+"\n"
+
+        for row in iterdata(table):
+            print row
+            l = [unicode(x).encode('utf-8','xmlcharrefreplace') for x in list(row)]
+            line =  ','.join(l)
+            yield line + '\n'
+    return Response(generate(), mimetype='text/html')
+    
+
+def mnch():
+    sql = "select * from project where id in (select project_id from initiative_project where initiative_id=1)"
+    table = fromdb(conn, sql)
+    table = cut(table, 'id','project_number')
+    foo =''
+    tocsv(table)
+    return foo
+    
+    print "=====99999999999====="
+    print foo
+    return tocsv(table)
+   
+    entries = [{"id" : row[0], "project_number" :row[3],"project_name":row[5],"url":row[4]} for row in result.fetchall()]
+    return jsonify(result=entries) 
+
 @app.route('/muskoka')
 def muskoka():
-    result=conn.execute("select * from project where id in (select project_id from initiative_project where initiative_id=1)")
-    entries = [{"id" : row[0], "project_number" :row[3],"project_name":row[5],"url":row[4]} for row in result.fetchall()]
-    return render_template('muskoka.html', entries=entries, title='Muskoka Initiative')
+    return render_template('recline.html', title='Muskoka Initiative')
 
 
 @app.route('/detail/<project_number>')
