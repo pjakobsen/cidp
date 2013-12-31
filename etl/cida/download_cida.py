3d8cfc322c731a55d2ee8b0596321f342533e0b8
Peder Jakobsen <pjakobsen@gmail.com>
2013-12-31 09:50:15 -0500

    DRYed script, dowloading to a more sensible location using os.path tools

--- etl/cida/download_cida.py
+++ etl/cida/download_cida.py
@@ -16,23 +16,30 @@ This visualization is based on two main data sources. The CIDA project browser (
 
 import os
 import urllib
+home = os.path.expanduser("~")
+data_dir = home+'/dev/cidp/data/'
+hpds_dir =data_dir+'/hpds/'
 
-urllib.urlretrieve("http://www.acdi-cida.gc.ca/cidaweb/cpo.nsf/vLUOpenDataFile/PBOpenData/\$file/Project%20Browser%20English.csv","browser.csv")
-print "OK Browser file"
-urllib.urlretrieve("http://www.acdi-cida.gc.ca/INET/IMAGES.NSF/vLUImages/Open%20Data/\$file/HPDS-2011-2012-eng.csv","hpds-2012.csv")
-print "OK 2012"
-urllib.urlretrieve("http://www.acdi-cida.gc.ca/INET/IMAGES.NSF/vLUImages/Open%20Data/\$file/HPDS-2010-2011-eng.csv","hpds-2011.csv")
-print "OK 2011"
-urllib.urlretrieve("http://www.acdi-cida.gc.ca/INET/IMAGES.NSF/vLUImages/Open%20Data/\$file/HPDS-2009-2010-eng.csv","hpds-2010.csv")
-print "OK 2010"
-urllib.urlretrieve("http://www.acdi-cida.gc.ca/INET/IMAGES.NSF/vLUImages/Open%20Data/\$file/HPDS-2008-2009-eng.csv","hpds-2009.csv")
-print "OK 2009"
-urllib.urlretrieve("http://www.acdi-cida.gc.ca/INET/IMAGES.NSF/vLUImages/Open%20Data/\$file/HPDS-2007-2008-eng.csv","hpds-2008.csv")
-print "OK 2008"
-urllib.urlretrieve("http://www.acdi-cida.gc.ca/INET/IMAGES.NSF/vLUImages/Open%20Data/\$file/HPDS-2006-2007-eng.csv","hpds-2007.csv")
-print "OK 2007"
+def save_file(url,filename):
+    path=None
+    if "browser" in filename:
+        path= os.path.normpath(data_dir+filename)
+    else:
+        path= os.path.normpath(hpds_dir+filename)
+    try:
+        urllib.urlretrieve(url, path)
+        print "OK file ", path
+    except:
+        raise
+    finally:
+        print "---- OK ----"
+    
+save_file("http://www.acdi-cida.gc.ca/cidaweb/cpo.nsf/vLUOpenDataFile/PBOpenData/\$file/Project%20Browser%20English.csv","browser.csv")
+save_file("http://www.acdi-cida.gc.ca/INET/IMAGES.NSF/vLUImages/Open%20Data/\$file/HPDS-2011-2012-eng.csv","hpds-2012.csv")
+save_file("http://www.acdi-cida.gc.ca/INET/IMAGES.NSF/vLUImages/Open%20Data/\$file/HPDS-2010-2011-eng.csv","hpds-2011.csv")
+save_file("http://www.acdi-cida.gc.ca/INET/IMAGES.NSF/vLUImages/Open%20Data/\$file/HPDS-2009-2010-eng.csv","hpds-2010.csv")
+save_file("http://www.acdi-cida.gc.ca/INET/IMAGES.NSF/vLUImages/Open%20Data/\$file/HPDS-2008-2009-eng.csv","hpds-2009.csv")
+save_file("http://www.acdi-cida.gc.ca/INET/IMAGES.NSF/vLUImages/Open%20Data/\$file/HPDS-2007-2008-eng.csv","hpds-2008.csv")
+save_file("http://www.acdi-cida.gc.ca/INET/IMAGES.NSF/vLUImages/Open%20Data/\$file/HPDS-2006-2007-eng.csv","hpds-2007.csv")
+save_file("http://www.acdi-cida.gc.ca/INET/IMAGES.NSF/vLUImages/Open%20Data/$file/HPDS-2005-2006-eng.csv", "hpds-2006.csv")
 
-urllib.urlretrieve("http://www.acdi-cida.gc.ca/INET/IMAGES.NSF/vLUImages/Open%20Data/$file/HPDS-2005-2006-eng.csv", 
-"hpds-2006.csv")
-print "OK 2006"
-print "ALL OK"
